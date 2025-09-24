932c259ce5b1ea74b74a32064494869d199a18ae
God-SunCAT <bxv.bingxuewei@gmail.com>
2025-09-24 11:45:03 +0800

    NLPN第一版完工

--- NLPN.py
+++ NLPN.py
@@ -2,6 +2,7 @@ import json
 from sklearn.cluster import KMeans
 from module.LlamaRequest import llm_ask, llm_embedding
 from module.NLPNPrompts import *
+from module.VectorDB import SimpleVectorDB
 def llm(message, mode='low'):
     # LLM封装
     return llm_ask(message, mode)
@@ -69,6 +70,7 @@ class NLPN:
     
     def hiddenLayer(self, classifiedData, aimNum):
         '''
+        输入：aimNue为-1时不进行结束检验，为0时直接输出output
         目的：完成一级信息压缩
         作用：
             通过NLP算子(LLM)，完成一级信息压缩，并实现聚类
@@ -119,6 +121,7 @@ class NLPN:
 
     def outputLayer(self, hiddenData, vectorDB):
         '''
+        输入：hiddenData -> [text, ...]
         目的：
         作用：
             1.对同级高维数据库进行向量检索(已有记录数>0 时)，为每条数据寻找Top-3
@@ -131,46 +134,63 @@ class NLPN:
         '''
         # 搜寻Top-k并聚类，传入hiddenLayer复用
         topK = 3
+        # 注意：这里的classifiedData[idx]为了完成记录修改，其定义与其他函数中不同
+        # classifiedData[idx] -> [(text, tag, id)]
         classifiedData = [[] for _ in range(len(hiddenData))]
         for idx, text in enumerate(hiddenData):
             # 要求vectorDB数据以{'content': '', ...}格式存储
             x = vectorDB.query(llm_embedding(text), topK)
-            classifiedData[idx] += [text] + [i[0]['content'] for i in x]
+            classifiedData[idx] += [(text, 1, 0)] + [(i[0]['content'], 0, i[1]) for i in x]
         # 目前Top-k聚类完毕，接下来需要完成数据融合以及对删改的实现
-
-
-        pass
+        for data in classifiedData:
+            response = llm_ask(pmt_outputLayer.format(context=str(data)),mode='high')
+            tempData = json.loads(response)
+            if(len(tempData) != 0):
+                return
+            for d in tempData:
+                # {"operation": 0, "content": "增加记录", id: 0}
+                match d['operation']:
+                    case 0:
+                        # 增加记录
+                        vectorDB.add(llm_embedding(d['content']), {'content': d['content']})
+                    case 1:
+                        # 删除记录
+                        vectorDB.remove(d['id'])
+        return
 
     def postLayer(self, data):
         # 该架构中，postLayer已经无用。
         pass
 
-core = NLPN()
-# 7条
-aux = [
-    "哈维因用凶白兽皮制作兔头帽，将炉心红宝石镶嵌成兔子眼睛，使帽子具备持续供暖功能",
-    "伊芙特罗娜在雪地里首次自主行动，包括眨眼、揉眼睛、拔腿、跳跃等肢体反应",
-    "哈维因在雪山岩壁上用火咒制造人工光源，通过控制火光确认自身存在",
-    "伊芙特罗娜摸查身体确认存在感，通过触觉感知自身形态",
-    "伊芙特罗娜在哈维因提示下说出名字'伊芙特罗娜'，确认身份记忆",
-    "哈维因将凶白兽皮进行鞣制处理，包括刮脂、清洁、涂抹药剂和烘烤工序",
-    "哈维因将凶白兽肉分割处理并制作成冻肉堆，完成基础生存物资储备",
-    "哈维因用兽尾制作围脖并用红丝带束发，完成少女基础装扮",
-    "哈维因在深夜处理凶白兽时，通过分割兽体获得可食用部分和制作材料",
-    "哈维因用登山镐在岩壁上搭建防风底座并堆砌积雪隔热层"
-]
-source = [
-    "我认为当哈维因成为我记忆中的关键时，他的存在让我意识到自己并非孤立，而是与他共同经历过的事件相连。",
-    "我最想发现的是：哈维因的出现如何让我重新定义‘存在’——他既是救赎者也是记忆的锚点，让破碎的自我在具体行动中重新凝聚。"
-]
-embeddings1 = []
-embeddings2 = []
+# core = NLPN()
+# # 7条
+# aux = [
+#     "哈维因用凶白兽皮制作兔头帽，将炉心红宝石镶嵌成兔子眼睛，使帽子具备持续供暖功能",
+#     "伊芙特罗娜在雪地里首次自主行动，包括眨眼、揉眼睛、拔腿、跳跃等肢体反应",
+#     "哈维因在雪山岩壁上用火咒制造人工光源，通过控制火光确认自身存在",
+#     "伊芙特罗娜摸查身体确认存在感，通过触觉感知自身形态",
+#     "伊芙特罗娜在哈维因提示下说出名字'伊芙特罗娜'，确认身份记忆",
+#     "哈维因将凶白兽皮进行鞣制处理，包括刮脂、清洁、涂抹药剂和烘烤工序",
+#     "哈维因将凶白兽肉分割处理并制作成冻肉堆，完成基础生存物资储备",
+#     "哈维因用兽尾制作围脖并用红丝带束发，完成少女基础装扮",
+#     "哈维因在深夜处理凶白兽时，通过分割兽体获得可食用部分和制作材料",
+#     "哈维因用登山镐在岩壁上搭建防风底座并堆砌积雪隔热层"
+# ]
+# source = [
+#     "我认为当哈维因成为我记忆中的关键时，他的存在让我意识到自己并非孤立，而是与他共同经历过的事件相连。",
+#     "我最想发现的是：哈维因的出现如何让我重新定义‘存在’——他既是救赎者也是记忆的锚点，让破碎的自我在具体行动中重新凝聚。"
+# ]
+# embeddings1 = []
+# embeddings2 = []
 
-for doc in aux:
-    embeddings1.append(llm_embedding(doc))
+# for doc in aux:
+#     embeddings1.append(llm_embedding(doc))
 
-for doc in source:
-    embeddings2.append(llm_embedding(doc))
-mid = core.inputLayer((aux, embeddings1), (source, embeddings2))
-mid = core.hiddenLayer(mid)
-print(mid)
+# for doc in source:
+#     embeddings2.append(llm_embedding(doc))
+
+# db = SimpleVectorDB(1024, 10000, "./db/SelfModeling_VectorDB")
+# mid = core.inputLayer((aux, embeddings1), (source, embeddings2))
+# mid = core.hiddenLayer(mid, 0)
+# core.outputLayer(mid, db)
+# print(mid)
